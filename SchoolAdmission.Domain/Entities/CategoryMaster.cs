fbc72759f8cd16aa3e906ab6ce5fdc770283a855
BhushanKK <bhushankachave9900@gmail.com>
2026-03-03 18:09:01 +0530

    Resolved CategoryMaster conflict

--- SchoolAdmission.Domain/Entities/CategoryMaster.cs
+++ SchoolAdmission.Domain/Entities/CategoryMaster.cs
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-=======
-using System.Text.Json.Serialization;
-
->>>>>>> 7907a0b3a15df033c9257aa96fdea1b30d1025fc
 namespace SchoolAdmission.Domain;
 
 public class CategoryMaster : AuditableEntity
 {
-<<<<<<< HEAD
     public int categoryId { get; set; }
     public string? Category { get; set; }
 }
-=======
-    [JsonIgnore]
-    public int CategoryId { get; set; }
-    public string? Category { get; set; }
-}
 
->>>>>>> 7907a0b3a15df033c9257aa96fdea1b30d1025fc
