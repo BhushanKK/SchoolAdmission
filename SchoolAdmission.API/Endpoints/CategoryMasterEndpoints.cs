ea71a29c626f45ddd1b2909ab76d484614180288
KunalP1117 <kunalpagare1117@gmail.com>
2026-03-04 21:19:47 +0530

    add Category id and Category

---
+++ SchoolAdmission.API/Endpoints/CategoryMasterEndpoints.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SchoolAdmission.Application.Features.CasteMasters.Commands;
+using SchoolAdmission.Application.Features.CasteMasters.Queries;
+
+namespace SchoolAdmission.API.Endpoints;
+
+public static class CategoryMasterEndpoints
+
+{
+    public ststic void MapCategoryMasterEndpoints(this IEndpointRouteBuilder app)
+    {
+       
+    }
+}
---
+++ SchoolAdmission.Application/Dtos/CategoryMasterdto.cs
@@ -0,0 +1,9 @@
+namespace SchoolAdmission.Application.Dtos;
+
+public class CategoryMasterdto
+{
+    public int   CategoryId {get; set;}
+    public String?  Category {get; set;}
+
+}
+
