c2f5d69ca67f87ea79b098318562bfe943693170
KunalP1117 <kunalpagare1117@gmail.com>
2026-03-04 23:05:43 +0530

    add create,update and delelte methods

--- SchoolAdmission.API/Endpoints/CategoryMasterEndpoints.cs
+++ SchoolAdmission.API/Endpoints/CategoryMasterEndpoints.cs
@@ -1,15 +1,96 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SchoolAdmission.Application.Features.CasteMasters.Commands;
-using SchoolAdmission.Application.Features.CasteMasters.Queries;
 
 namespace SchoolAdmission.API.Endpoints;
 
 public static class CategoryMasterEndpoints
-
 {
-    public ststic void MapCategoryMasterEndpoints(this IEndpointRouteBuilder app)
+    public static void MapCategoryMasterEndpoints(this IEndpointRouteBuilder app)
     {
-       
+        var group = app.MapGroup("/api/categorymasters");
+
+        // GET ALL
+        group.MapGet("/", async (IMediator mediator) =>
+        {
+            var result = await mediator.Send(new GetAllCategoryMastersQuery());
+            return Results.Ok(result);
+        });
+
+        // GET BY ID
+        group.MapGet("/{id:int}", async (int id, IMediator mediator) =>
+        {
+            var result = await mediator.Send(new GetCategoryMasterByIdQuery(id));
+
+            return result is null
+                ? Results.NotFound("Category not found")
+                : Results.Ok(result);
+        });
+
+        // CREATE
+        group.MapPost("/", async ([FromBody] CreateCategoryMasterCommand command, IMediator mediator) =>
+        {
+            var id = await mediator.Send(command);
+
+            return Results.Ok(new
+            {
+                Success = true,
+                Message = "Category created successfully",
+                Data = id
+            });
+        });
+
+        // UPDATE
+        group.MapPut("/{id:int}", async (int id, [FromBody] UpdateCategoryMasterCommand command, IMediator mediator) =>
+        {
+            command.CategoryId = id;
+            var success = await mediator.Send(command);
+
+            return (bool)success
+                ? Results.Ok("Category updated successfully")
+                : Results.NotFound("Category not found");
+        });
+
+        // DELETE
+        group.MapDelete("/{id:int}", async (int id, IMediator mediator) =>
+        {
+            var success = await mediator.Send(new DeleteCategoryMasterCommand(id));
+
+            return success
+                ? Results.Ok("Category deleted successfully")
+                : Results.NotFound("Category not found");
+        });
     }
+
+    private class GetCategoryMasterByIdQuery : IRequest<object?>
+    {
+        public GetCategoryMasterByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+    }
+}
+
+internal class DeleteCategoryMasterCommand : IRequest<bool>
+{
+    private int id;
+
+    public DeleteCategoryMasterCommand(int id)
+    {
+        this.id = id;
+    }
+}
+
+internal class UpdateCategoryMasterCommand
+{
+    public int CategoryId { get; internal set; }
+}
+
+internal class CreateCategoryMasterCommand
+{
+}
+
+internal class GetAllCategoryMastersQuery : IRequest<object?>
+{
 }
