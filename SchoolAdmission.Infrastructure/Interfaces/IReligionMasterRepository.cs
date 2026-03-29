50039b233764790deee826ebff49d5cb7715daac
BhushanKK <bhushankachave9900@gmail.com>
2026-03-29 09:10:37 +0530

    refactor caste repo interface

--- SchoolAdmission.Infrastructure/Interfaces/IReligionMasterRepository.cs
+++ SchoolAdmission.Infrastructure/Interfaces/IReligionMasterRepository.cs
@@ -7,8 +7,8 @@ public interface IReligionMasterRepository
     Task<List<ReligionMaster>> GetAllAsync(CancellationToken cancellationToken);
     Task<ReligionMaster?> GetByIdAsync(int id, CancellationToken cancellationToken);
     Task AddAsync(ReligionMaster religion , CancellationToken cancellationToken);
-    Task UpdateAsync(ReligionMaster religion, CancellationToken cancellationToken);
-    Task DeleteAsync(ReligionMaster religion, CancellationToken cancellationToken);
+    Task UpdateAsync(ReligionMaster religion);
+    Task DeleteAsync(ReligionMaster religion);
     Task<bool> IsExistsAsync(string Religion, OperationType operation, int? ReligionId, CancellationToken cancellationToken);
 }
 
